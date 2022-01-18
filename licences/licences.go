63d3a4047f9e437fa9d84722c831ee0076f83231
Jonathan Poole <jpoole@thoughtmachine.net>
2022-01-18 17:50:22 +0000

    Fix

--- licences/licences.go
+++ licences/licences.go
@@ -1,6 +1,7 @@
 package licences
 
 import (
+	"fmt"
 	"github.com/tatskaari/go-deps/resolve"
 	"golang.org/x/tools/go/packages"
 )
@@ -11,7 +12,7 @@ func SetLicences(modules *resolve.Modules, driver packages.Driver) error {
 		for _, p := range m.Parts {
 			p.Modified = true
 		} // So the licences actually get set
-		paths = append(paths, m.Name)
+		paths = append(paths, fmt.Sprintf("%v@%v", m.Name, m.Version))
 	}
 
 	pkgs, r, err := resolve.Load(paths, driver)
