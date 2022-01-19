8c7957f33b28acdcfa35363b71625ee9e1850dec
Jonathan Poole <jpoole@thoughtmachine.net>
2022-01-19 10:15:46 +0000

    best effort

--- licences/licences.go
+++ licences/licences.go
@@ -7,9 +7,16 @@ import (
 	"github.com/tatskaari/go-deps/resolve/driver"
 	"github.com/tatskaari/go-deps/resolve/model"
 	"golang.org/x/tools/go/packages"
+	"os"
+	"path/filepath"
 )
 
 func SetLicences(modules *resolve.Modules, driver *driver.PleaseDriver) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
 	var paths = map[string]*model.Module{}
 	for _, m := range modules.Mods {
 		for _, p := range m.Parts {
@@ -26,7 +33,7 @@ func SetLicences(modules *resolve.Modules, driver *driver.PleaseDriver) error {
 	c, _ := licenses.NewClassifier(0.9)
 
 	for root, m := range modules.Mods {
-		licence, _, err := c.Identify(root)
+		licence, _, err := c.Identify(filepath.Join(wd, "plz-out/godeps/go", root))
 		if err != nil {
 			return fmt.Errorf("failed to identify licence ofr %s: %v", root, err)
 		}
